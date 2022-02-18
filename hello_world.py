6f53a29a990ea3e32f48f21ac995b916b32faca6
Wilmer Diaz <wdiazplus@Wilmers-Mac-mini.local>
2022-02-18 01:07:35 -0500

    Delete package related with Google browser

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,6 @@ import unittest
 import time
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
